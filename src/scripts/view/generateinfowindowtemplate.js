d00dcc3d5ecd201312b8ce20c97ab8d3637b8f27
John Wu <johnwu93@gmail.com>
2017-12-16 13:09:54 -0800

    feature: refined the responsiveness of the infowindow more

--- src/scripts/view/generateinfowindowtemplate.js
+++ src/scripts/view/generateinfowindowtemplate.js
@@ -7,23 +7,29 @@ const generateInfoWindowTemplate = function generateInfoWindowTemplate(business)
   const [street, city] = address.split(/,(.*)/);
   const {text, url} = review;
   return `<div class="info-window-container">
-      <div class="row">
-        <section class="col s6">
+      <div class="row flex">
+        <section class="col s12 l6">
           <h5>
             <a href="#">${name}</a>
           </h5>
           <article class="info-text">
             <div class="btn">${rating}</div>
-            <div>${street}</div>
-            <div>${city}</div>
+            <div class="hide-on-med-and-down">${street}</div>
+            <div class="hide-on-med-and-down">${city}</div>
+            <h6 class="hide-on-large-only">
+              <a href="${url}">Read Review</a>
+            </h6>
           </article>
         </section>
-        <section class="col s6">
-          <img src="${photo}" alt="">
+        <section class="col l6 hide-on-med-and-down">
+          <img src="${photo}" class="materialboxed responsive-img" alt="">
         </section>
       </div>
-      <article class="truncate"> Review: ${text} </article>
-      <a href="${url}">Read More</a>
+      <section class="hide-on-med-and-down">
+        <article class="truncate">
+          <a class="review" href="${url}"><i class="material-icons">comment</i></a> ${text} 
+         </article>
+      </section>
    </div>`;
 };
 
